1e04d9094419be552db613aec52ddcccb42a87ae
Agustinus Theodorus <agustinus.theodorus@binus.edu>
2019-12-28 04:08:53 +0700

    Update: added pickadate.js library

--- resources/views/layout/manage.blade.php
+++ resources/views/layout/manage.blade.php
@@ -10,6 +10,8 @@
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/pickadate.js/3.6.4/compressed/themes/default.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/pickadate.js/3.6.4/compressed/themes/default.date.css" />
     </head>
     <body>
         <nav class="bg-dark p-1">
@@ -22,9 +24,9 @@
                         <button class="dropbtn">Manage
                           <i class="fa fa-caret-down"></i>
                         </button>
-                        @if(Auth::user()->name == 'Admin')
+                        @if(Auth::user()->role === 1)
                             <div class="dropdown-content">
-                              <a href="">Manage Question</a>
+                              <a href="{{route('view-all-question')}}">Manage Question</a>
                               <a href="{{route('view-all-user')}}">Manage User</a>
                               <a href="{{route('view-all-topic')}}">Manage Topic</a>
                             </div>
@@ -72,6 +74,8 @@
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/pickadate.js/3.6.4/compressed/picker.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/pickadate.js/3.6.4/compressed/picker.date.js"></script>
         <script src="{{ URL::asset('js/application.js') }}"></script>
         <div class="container main-container">
             @yield('content')
